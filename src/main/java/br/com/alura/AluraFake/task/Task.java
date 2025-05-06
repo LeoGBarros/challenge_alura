b4fdf0b105a508ae7b84671393de1f500000ab3b
Leonardo Barros <leoguedesbarros@gmail.com>
2025-05-06 15:17:27 -0300

    feat(task): add base Task entity and DTO for open text activity creation

---
+++ src/main/java/br/com/alura/AluraFake/task/Task.java
@@ -0,0 +1,58 @@
+package br.com.alura.AluraFake.task;
+
+import br.com.alura.AluraFake.course.Course;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "task_type")
+public abstract class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 4, max = 255)
+    private String statement;
+
+    @NotNull
+    private Integer orderIndex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Deprecated
+    protected Task() {
+    }
+
+    public Task(String statement, Integer orderIndex, Course course) {
+        this.statement = statement;
+        this.orderIndex = orderIndex;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public Integer getOrderIndex() {
+        return orderIndex;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setOrderIndex(Integer newOrder) {
+        this.orderIndex = newOrder;
+    }
+}
